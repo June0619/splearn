05fcaabe977d35ce3f945c1743374c4d717970d9
June0619 <jjw5050@hotmail.com>
2026-07-27 12:16:59 +0900

    회원 애플리케이션 포트 정의

--- src/main/java/me/jwjung/splearn/application/required/MemberRepository.java
+++ src/main/java/me/jwjung/splearn/application/required/MemberRepository.java
@@ -3,6 +3,9 @@ package me.jwjung.splearn.application.required;
 import me.jwjung.splearn.domain.Member;
 import org.springframework.data.repository.Repository;
 
+/**
+ * 회원 정보를 저장하거나 조회한다
+ */
 public interface MemberRepository extends Repository<Member, Long> {
     Member save(Member member);
 }
